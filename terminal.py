a5236976add199525730539e8d111bcb50e304e7
Walker Inman <winman@lucidsci.com>
2015-07-24 14:00:13 -0400

    working pyusb terminal for ftdi device

--- terminal.py
+++ terminal.py
@@ -18,9 +18,9 @@ import threading
 import logging
 
 try:
-  import Queue
+  import Queue as queue
 except ImportError:
-  import queue as Queue
+  import queue
 import os
 if os.name == "nt":
   import msvcrt
@@ -33,9 +33,59 @@ log_fmt = logging.Formatter(
 fileHandler.setFormatter( log_fmt )
 log.addHandler( fileHandler )
 
+
+# NOTE: getch taken from http://stackoverflow.com/questions/510357/python-read-a-single-character-from-the-user
+class _Getch(object):
+  """ Gets a single character from standard input.
+      Does not echo to the screen.
+  """
+  def __init__(self):
+    try:
+      self.impl = _GetchWindows()
+    except ImportError:
+      self.impl = _GetchUnix()
+
+  def __call__(self):
+    char = self.impl()
+    if char == '\x03':
+      raise KeyboardInterrupt
+    elif char == '\x04':
+      raise EOFError
+    return char
+
+class _GetchUnix(object):
+  def __init__(self):
+    import tty
+    import sys
+
+  def __call__(self):
+    import sys
+    import tty
+    import termios
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr( fd )
+    try:
+      tty.setraw( sys.stdin.fileno() )
+      ch = sys.stdin.read( 1 )
+    finally:
+      termios.tcsetattr( fd, termios.TCSADRAIN, old_settings )
+    return ch
+
+class _GetchWindows(object):
+  def __init__(self):
+    import msvcrt
+
+  def __call__(self):
+    import msvcrt
+    return msvcrt.getch()
+
+getch = _Getch()
+
+
 class ComPort( object ):
   def __init__( self, usb_device ):
     self.device = usb_device
+    self._isFTDI = False
 
     cfg = usb_device.get_active_configuration()
 
@@ -57,11 +107,8 @@ class ComPort( object ):
       if( len(cmd_itfs) != len(data_itfs) ):
         log.debug( "COM port data / command interface mismatch" )
 
-
     ports = len( data_itfs )
     log.debug( "found {0} COM port\n".format(ports) )
-
-#    for i, cmd_itf, data_itf in zip( xrange(len(data_itfs)), cmd_itfs, data_itfs ):
 
     try:
       self.device.detach_kernel_driver( itf_num )
@@ -78,23 +125,21 @@ class ComPort( object ):
   def write( self, data ):
     ret = self.ep_out.write( data )
     if( len(data) != ret ):
-      print( "Bytes written mismatch {0} vs {1}".format(len(data), ret) )
+      log.error( "Bytes written mismatch {0} vs {1}".format( len(data), ret)  )
 
   def read( self, timeout = None, attempts = 4 ):
     ret = []
     size = self.ep_in.wMaxPacketSize
-    if timeout is None:
-      for i in range(attempts):
-        try:
-          ret += self.ep_in.read( size )
-        except usb.USBError:
-          pass
-    else:
-      for i in range(attempts):
-        try:
-          ret += self.ep_in.read( size, timeout )
-        except usb.USBError:
-          pass
+    kwargs = dict() if timeout is None else { "timeout":timeout }
+    for i in range( attempts ):
+      try:
+        rv = self.ep_in.read( size, **kwargs )
+        if self._isFTDI:
+          status = rv[:2]       # FTDI prepends 2 flow control characters
+          rv = rv[2:]
+        ret += rv
+      except usb.USBError:
+        pass
     return ret
 
   def read_text( self, timeout = None, attempts = 4 ):
@@ -104,7 +149,7 @@ def selectDevice( ):
   devices = list( usb.find( find_all=True ) )
 
   if len( devices ) == 0:
-    print("No devices detected")
+    print( "No devices detected" )
     return None
 
   selection = -1
@@ -116,10 +161,10 @@ def selectDevice( ):
         manufacturer = d.manufacturer
       except:
         manufacturer = "Unknown"
-      print ("%d: %04x:%04x on Bus %03d Address %03d %s" % (i, d.idVendor,
-        d.idProduct, d.bus, d.address, manufacturer))
+      print( "%d: %04x:%04x on Bus %03d Address %03d %s" % (i, d.idVendor,
+        d.idProduct, d.bus, d.address, manufacturer) )
 
-    selection = input("Enter device: ")
+    selection = input( "Enter device: " )
 
     try:
       selection = int( selection )
@@ -132,7 +177,7 @@ def selectDevice( ):
   #try:
   d = devices[selection]
   #except Exception as e:
-  #  print("Cannot open com port for {}".format(str(d)))
+  #  log.error("Cannot open com port for {}".format(str(d)))
   #  print(str(e))
   #  exit()
 
@@ -140,12 +185,8 @@ def selectDevice( ):
 
 def readinput( queue ):
   while True:
-    if os.name == "nt":
-      command = msvcrt.getch()
-    else:
-      command = sys.stdin.read(1)
-    queue.put( command )
-    #time.sleep( 1 )
+    queue.put( getch() )
+
 
 if __name__ == '__main__':
   d = selectDevice(  )
@@ -155,19 +196,23 @@ if __name__ == '__main__':
 
   p = ComPort(d)
 
-  input_queue = Queue.LifoQueue()
-
+  input_queue = queue.Queue()
   input_thread = threading.Thread( target=lambda : readinput( input_queue ) )
   input_thread.daemon = True
   input_thread.start()
 
   while True:
+    time.sleep( 0.2 )
     text = p.read_text( timeout=1 )
     if len( text ):
-      print(text)
+      valid = set( list(range(ord(' '), ord('~') + 1)) + [ ord('\r'), ord( '\n' ) ] )
+      print(
+          "".join( "\\x{:02X}".format( ord(c) ) if ord(c) not in valid else c for c in text  ),
+          end="" )
 
     if not input_queue.empty():
       command = input_queue.get()
+      print( command, end="" )
       p.write(command)
 
 
